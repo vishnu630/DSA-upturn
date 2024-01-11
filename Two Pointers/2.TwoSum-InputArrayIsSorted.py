7953dff9cb5c66217b66706eb26bc4424cb2354b
Vishnu Teja Reddy <vishnuteja779986@gmail.com>
2024-01-11 08:23:50 +0000

    Two Sum O(n)

---
+++ Two Pointers/2.TwoSum-InputArrayIsSorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left,right=0,len(numbers)-1
+
+        while left < right:
+            currentSum= numbers[left] + numbers[right]
+            if currentSum > target:
+                right -= 1
+            elif currentSum < target:
+                left +=1
+            else:
+                return [left+1,right+1]
+        return []
+        
